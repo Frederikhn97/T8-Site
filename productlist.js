f98a24d254b42b440d5ee7f6fa1465a8b6c25f0e
sebastian2698 <sebl0002@stud.ek.dk>
2026-03-18 10:58:27 +0100

    productlist3

--- productlist.js
+++ productlist.js
@@ -1,5 +1,5 @@
-const container = document.querySelector(".productlist");
-const categories = ["smartphones", "laptops", "tablets", "mobile-accessories"];
+const container = document.querySelector(".products");
+const categories = ["Smartphones", "Laptops", "Tablets", "Mobile-accessories"];
 
 categories.forEach((cat) => {
   fetch(`https://dummyjson.com/products/category/${cat}`)
@@ -8,19 +8,43 @@ categories.forEach((cat) => {
     .catch((err) => console.error(err));
 });
 
-function showProducts(category, products) {
-  let html = `<h2>${category}</h2>`; // valgfrit: titel for kategorien
+function createStarRating(rating) {
+  let stars = "";
 
+  for (let i = 1; i <= 5; i++) {
+    if (rating >= i) {
+      stars += `<span class="star full"></span>`;
+    } else if (rating >= i - 0.5) {
+      stars += `<span class="star half"></span>`;
+    } else {
+      stars += `<span class="star empty"></span>`;
+    }
+  }
+
+  return stars + ` (${rating})`;
+}
+
+function showProducts(category, products) {
+  document.querySelector(".tekst").textContent = category;
+  document.querySelector(".stortudvalg").textContent = `Se det største udvalg af ${category} fra de største brands i verden.`;
+  let html = ``;
+  html += `<div class="productlist">`;
   products.forEach((product) => {
     html += `
-      <article class="product">
-        <img src="${product.thumbnail}">
-       
-        <p class="price">${product.price},-</p>
-        <p>${product.description}</p>
+        <article class="product">
+       <img src="${product.thumbnail}">
+      
+ <h4 class=title>${product.title}</h4>
+      <p class="rating">
+ ${createStarRating(product.rating)}
+</p>
+       <p class="price">${product.price},-</p>
+        <p class=description>${product.description}</p>
+        
       </article>
+      
     `;
   });
-
-  container.innerHTML += html; // += så alle kategorier vises
+  html += `</div>`;
+  container.innerHTML += html;
 }
